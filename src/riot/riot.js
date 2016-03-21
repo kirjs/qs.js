8e0a9ff92b78793b8c8d9fb2cfd43482c3a799ec
Roman Akhmadullin <freeman.aka.ahmed@gmail.com>
2016-03-21 14:45:21 +0500

    [feat] riot.js quick start

---
+++ src/riot/riot.js
@@ -0,0 +1,7 @@
+riot.mount('riot-component', {
+    username: 'Peter',
+    tasks: [
+        { title: 'clean cup', completed: false },
+        { title: 'go to sleep', completed: false },
+    ]
+});
