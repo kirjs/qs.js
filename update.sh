cd6185ca29637aa3a29fea298a47b92f90878d2f
Kirill Cherkashin <kirill@kcherkashin.org>
2016-07-30 16:08:10 -0400

    Add update.sh file

---
+++ update.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# This script builds the site
+#
+
+# clean up
+cd "$(git rev-parse --show-toplevel)"
+set -e
+rm -rf dist || exit 0;
+mkdir dist
+
+# build
+cp ./site/* ./dist/
+babel-node site-src/update.js
+cd dist
+
+
