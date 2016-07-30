72c0ca83fa543311bc14339402183508ef529cc3
Kirill Cherkashin <kirill@kcherkashin.org>
2016-07-29 21:40:53 -0400

    Make d3.js work with es 5

--- src/d3/d3.js
+++ src/d3/d3.js
@@ -1,9 +1,11 @@
 var svg = d3.select("svg");
 
-var circle = svg.selectAll("text")
+svg.selectAll("text")
   .data(['Hello, World!'])
   .enter()
   .append("text")
   .attr("dx", 10)
   .attr("dy", 10)
-  .text(a=>a);
+  .text(function (a){
+    return a
+  });
