a85012295d88a47277ef23ed6b5a690b86b0eddf
Kirill Cherkashin <kirill@kcherkashin.org>
2016-07-31 14:21:51 -0400

    Add sauce number

--- nightwatch.js
+++ nightwatch.js
@@ -1,3 +1,5 @@
+const TRAVIS_JOB_NUMBER = process.env.TRAVIS_JOB_NUMBER
+
 module.exports = {
   "src_folders": [
     "tests/e2e"
@@ -14,6 +16,8 @@ module.exports = {
         browserName: "chrome",
         platform: "OS X 10.11",
         version: "47",
+        build: `build-${TRAVIS_JOB_NUMBER}`,
+        'tunnel-identifier': TRAVIS_JOB_NUMBER,
       },
       "launch_url": "http://127.0.0.1:8080/",
       "selenium_port": 80,
