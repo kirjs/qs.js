4458727d30fd0df0c636539d5e124d28e816e881
Kirill Cherkashin <kirill@kcherkashin.org>
2016-07-31 11:41:59 -0400

    Fix extracting lib versions when updating

--- site-src/update.js
+++ site-src/update.js
@@ -2,31 +2,80 @@ var getData = require('./getData');
 var data = getData.read();
 var utils = require('./utils');
 var cloudFlareRegex = /\/\/cdnjs.cloudflare.com\/ajax\/libs\/.+?\/.+?\//g;
-var libRegex = /\/\/cdnjs.cloudflare.com\/ajax\/libs\/(.+?)\/(.+?)\//g;
 
+var extractLib = function extractLib(html){
+  var libRegex = /\/\/cdnjs.cloudflare.com\/ajax\/libs\/(.+?)\/(.+?)\//;
+  return (html.match(cloudFlareRegex) || []).reduce((libs, lib)=>{
+    const [full, name, version] = lib.match(libRegex);
+    libs.push({name, version});
+    return libs;
+  }, []);
+};
 module.exports = {
-  extractLib: function extractLib(html){
-    return (item.data.html.match(cloudFlareRegex) || []).reduce((libs, lib)=>{
-      const [full, name, version] = lib.match(libRegex);
-      console.log(lib, lib, name, version, '*', lib.match(libRegex));
-      libs.push({name, version});
-      return libs;
-    }, libs);
-  }
+  extractLib
 };
 
+
+/**
+ * Step 1: extract all libs and versions from html
+ * @type {{
+ *    name: string,
+ *   version: string
+ *   }}
+ */
 var libs = data.reduce((libs, group)=>{
   return group.items.reduce((libs, item)=>{
-
+    return libs.concat(extractLib(item.data.html));
   }, libs)
 }, []);
 
+
+/**
+ * Step 2:
+ * - Remove duplicates.
+ * - Remove angular, as the versions are messed up.
+ */
+var map = {};
+libs = libs.filter(lib=>lib.name !== 'angular.js' && !map[lib.name] && (map[lib.name] = true));
+
+
+/**
+ * Step3. Fetch the latest versions.
+ */
+if (false) {
+  Promise.all(libs.map((lib)=>{
+    return utils.getContent(`https://api.cdnjs.com/libraries?search=${lib.name}&fields=version`);
+  })).then((result)=>{
+
+    var latestVersions = libs.reduce((map, {name}, i)=>{
+      map[name] = result[i].results.filter(n=> n.name === name)[0].version;
+      return map;
+    }, {});
+    console.log(latestVersions);
+// updateTheLibs(libs, latestVersions);
+
+  }, ()=>{
+    console.log(arguments)
+  }).catch((a)=>{
+    console.log(a)
+  });
+}
+
+
+/**
+ * Eventually update the libs in html.
+ * @param libs
+ * @param latestVersions
+ */
+
 function updateTheLibs(libs, latestVersions){
+  var libRegex = /\/\/cdnjs.cloudflare.com\/ajax\/libs\/(.+?)\/(.+?)\//g;
   data.reduce((libs, group)=>{
     return group.items.reduce((libs, item)=>{
       if (item.data.html.match(cloudFlareRegex)) {
+
         item.data.html = item.data.html.replace(libRegex, function (full, name, version){
-          return `//cdnjs.cloudflare.com/ajax/libs/${name}/${latestVersions[name]||version}/`;
+          return `//cdnjs.cloudflare.com/ajax/libs/${name}/${latestVersions[name] || version}/`;
         });
 
         getData.update(item);
@@ -35,33 +84,23 @@ function updateTheLibs(libs, latestVersions){
   }, []);
 }
 
-var latest = {
-  'angular.js': '2.0.0-beta.17',
+var latestVersion = {
   rxjs: '4.1.0',
+  'cyclejs-core': '7.0.0',
+  'cyclejs-dom': '10.1.0',
   normalize: '4.2.0',
   redux: '3.5.2',
+  'react-redux': '4.4.5',
   riot: '2.5.0',
   vue: '1.0.26',
   c3: '0.4.11',
   'processing.js': '1.6.0',
+  react: '15.2.1',
   highcharts: '4.2.5',
+  'react-highcharts': '9.0.0',
   'tabletop.js': '1.4.3',
-  mocha: '3.0.0-2'
+  mocha: '3.0.0-2',
+  chai: '3.5.0'
 };
 
-
-//updateTheLibs(libs, latest);
-console.log(libs);
-/*
- Promise.all(libs.map((lib)=>{
- return utils.getContent(`https://api.cdnjs.com/libraries?search=${lib.name}&fields=version`);
- })).then((result)=>{
-
- var latestVersions = libs.reduce((map, {name}, i)=>{
- map[name] = result[i].results.filter(n=> n.name === name)[0].version;
- return map;
- }, {});
-
- updateTheLibs(libs, latestVersions);
-
- }, ()=>{console.log(arguments)}).catch((a)=>{console.log(a)});*/
+updateTheLibs(libs, latestVersion);
--- tests/unit/update.spec.js
+++ tests/unit/update.spec.js
@@ -2,9 +2,34 @@ var update = require('../../site-src/update');
 var assert = require('assert');
 
 describe('update', () =>{
-  describe('extract dependencies', ()=>{
-    it('updates', function (){
-      assert(true);
+  describe('Library version extraction', ()=>{
+    it('does not fail if there are no supported libraries', function (){
+      assert.deepEqual(update.extractLib('<html></html>'), []);
+    });
+
+    it('extract single cdnjs dependency', function (){
+      assert.deepEqual(update.extractLib(
+        `<html>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/react/0.14.1/react.js"></script>
+        </html>`), [{name: 'react', version: '0.14.1'}]);
+    });
+
+    it('extract multiple cdnjs dependencies', function (){
+      assert.deepEqual(update.extractLib(
+        `<html>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/react/0.14.1/react.js"></script>
+            blabla 
+            <script src = "https://cdnjs.cloudflare.com/ajax/libs/react-highcharts/7.0.0/ReactHighcharts.js"></script>
+        </html>`), [
+        {
+          name: 'react',
+          version: '0.14.1'
+        },
+        {
+          "name": "react-highcharts",
+          "version": "7.0.0"
+        }
+      ]);
     });
   });
 });
